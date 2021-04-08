a5b02e892b370a4483ac45cec8a2676b487bf9bb
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-08 14:46:51 +0300

    updated users controller

--- srcs/app/srcs/app/controllers/api/users_controller.rb
+++ srcs/app/srcs/app/controllers/api/users_controller.rb
@@ -1,10 +1,47 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user!
+  #  before_action :authenticate_user!
+  before_action :define_filters
+  before_action :find_user, only: %i[show update destroy]
+
   # GET /api/users.json
   def index
     @users = User.all
-    respond_to do |format|
-      format.json { render json: @users, only: %i[id nickname email wins loses displayname] }
+    render json: @users, only: @filters
+  end
+
+  # GET /api/users/id.json
+  def show
+    render json: @user, only: @filters
+  end
+
+  # PATCH/PUT /api/users/id.json
+  def update
+    if @user.update(user_params)
+      render :show, status: :ok, location: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
   end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def find_user
+    @user = if params[:id] == 'current' # /api/users/current.json
+              current_user
+            else
+              User.find(params[:id])
+            end
+  end
+
+  # DRY filters for json responses
+  def define_filters
+    @filters = %i[id nickname email wins loses displayname]
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(@filters)
+  end
+
 end
