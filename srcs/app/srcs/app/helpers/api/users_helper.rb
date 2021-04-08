93d7e2c38e7316a7d49bd152f409ee911fa11f54
Antip003 <ant.sergy@gmail.com>
2021-04-08 15:20:37 +0300

    image start

--- srcs/app/srcs/app/helpers/api/users_helper.rb
+++ srcs/app/srcs/app/helpers/api/users_helper.rb
@@ -1,2 +1,7 @@
 module Api::UsersHelper
+    def user_gravatar
+        gravatar_id = Digest::MD5::hexdigest(current_user.email.downcase)
+        gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?d=identicon"
+        image_tag(gravatar_url, alt: current_user.nickname, class: "gravatar")
+    end
 end
