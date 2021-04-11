382c964fee3da85deeef7ddba98f5562ee071229
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-11 04:03:33 +0300

    added admin column to users

--- srcs/app/srcs/db/schema.rb
+++ srcs/app/srcs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_08_160903) do
+ActiveRecord::Schema.define(version: 2021_04_10_234739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_04_08_160903) do
     t.binary "avatar"
     t.string "avatar_url"
     t.string "avatar_default_url"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider"], name: "index_users_on_provider"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
