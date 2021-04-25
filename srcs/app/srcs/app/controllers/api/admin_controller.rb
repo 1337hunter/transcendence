0a2cc09a3febfde0c4807b0291eec47ea8057fee
Ivan <thefullarcticfox@users.noreply.github.com>
2021-04-25 19:52:51 +0300

    admin controller fix

--- srcs/app/srcs/app/controllers/api/admin_controller.rb
+++ srcs/app/srcs/app/controllers/api/admin_controller.rb
@@ -5,7 +5,8 @@ class Api::AdminController < ApplicationController
   before_action :check_2fa
   before_action :check_admin
   before_action :define_filters
-  before_action :find_user, only: %i[]
+  before_action :find_user, only: %i[user_update]
+  before_action :find_chat, only: %i[chat_update]
 
   # GET /api/admin/users.json
   def users
@@ -28,7 +29,29 @@ class Api::AdminController < ApplicationController
     render json: @rooms, only: @roomfilters
   end
 
+  # PATCH /api/admin/users/id.json
+  def user_update
+    if @user.update(user_params)
+      render json: @user, only: @userfilters, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH /api/admin/users/id.json
+  def chat_update
+    if @room.update(room_params)
+      render json: @room, only: @roomfilters, status: :ok
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def check_admin
+    render json: {error: "You have no permission"}, status: :forbidden unless current_user.admin?
+  end
 
   def define_filters
     @userfilters = %i[id nickname displayname email admin banned avatar_url avatar_default_url]
@@ -39,7 +62,15 @@ class Api::AdminController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def check_admin
-    render json: {error: "You have no permission"}, status: :forbidden unless current_user.admin?
+  def find_chat
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:admin).permit(%i[displayname avatar_url admin banned])
+    end
+
+  def room_params
+    params.require(:admin).permit(%i[name privates])
   end
 end
