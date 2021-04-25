1d914bcadfc0b1779478d15b508098504be629ac
Mark <mskoromec@gmail.com>
2021-04-25 19:47:26 +0300

    little message controller fix

--- srcs/app/srcs/app/controllers/api/messages_controller.rb
+++ srcs/app/srcs/app/controllers/api/messages_controller.rb
@@ -1,7 +1,21 @@
 class Api::MessagesController < ApplicationController
     skip_before_action :verify_authenticity_token
 
+    def index
+        @messages = Message.all
+        render json: @messages
+    end
+
     def create
+        @message = Message.new(message_params)
+        @message.user = current_user
+        @message.save
         ActionCable.server.broadcast("chat_room", { body: params['content'] })
+    end
+
+    private
+    
+    def message_params
+        params.require(:message).permit(:content)
     end
 end
