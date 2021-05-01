6adcf72cef741f28b39be22f761f887f67fa9c39
Mark <mskoromec@gmail.com>
2021-05-01 22:34:38 +0300

    fail fix

--- srcs/app/srcs/db/schema.rb
+++ srcs/app/srcs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_28_122039) do
+ActiveRecord::Schema.define(version: 2021_05_01_191941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,9 +18,10 @@ ActiveRecord::Schema.define(version: 2021_04_28_122039) do
   create_table "messages", force: :cascade do |t|
     t.string "content"
     t.bigint "user_id", null: false
-    t.integer "room_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "room_id", null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -32,6 +33,8 @@ ActiveRecord::Schema.define(version: 2021_04_28_122039) do
     t.boolean "private"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "message_id"
+    t.index ["message_id"], name: "index_rooms_on_message_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -67,5 +70,7 @@ ActiveRecord::Schema.define(version: 2021_04_28_122039) do
     t.index ["uid"], name: "index_users_on_uid"
   end
 
+  add_foreign_key "messages", "rooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "rooms", "messages"
 end
