7153a9cf6427b47f0e6991a7d6e47971b897e1bd
Antip003 <ant.sergy@gmail.com>
2021-05-03 14:02:35 +0300

    room styling

--- srcs/app/srcs/db/schema.rb
+++ srcs/app/srcs/db/schema.rb
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_223333) do
     t.bigint "message_id"
     t.index ["message_id"], name: "index_rooms_on_message_id"
   end
+  execute "SELECT setval('rooms_id_seq', 4)"
 
   create_table "users", force: :cascade do |t|
     t.string "displayname"
