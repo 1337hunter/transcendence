e3038b2461aa8a5fef780c0d673cbdd13e3e45af
Cobbler Asteria <casteria@ox-l3.msk.21-school.ru>
2021-05-16 19:40:19 +0300

    fix

--- srcs/app/srcs/app/javascript/channels/game_room_channel.js
+++ srcs/app/srcs/app/javascript/channels/game_room_channel.js
@@ -10,26 +10,6 @@ var obtainedValues =
 
 var GameRoomInit = 
 {
-<<<<<<< HEAD
-    GameRoom: async function () {
-        const Room = consumer.subscriptions.create("GameRoomChannel", {
-        connected() {
-          // Called when the subscription is ready for use on the server
-          console.log("Connected to game room channel");
-        },
-      
-        disconnected() {
-          // Called when the subscription has been terminated by the server
-        },
-      
-        received(data) {
-            console.log(data);
-          // Called when there's incoming data on the websocket for this channel
-        }
-    });
-    return Room;
-  }
-=======
     $this: this,
     createGameRoom: function ($this) {
         const GameRoom = consumer.subscriptions.create("GameRoomChannel", {
@@ -39,6 +19,7 @@ var GameRoomInit =
             },
 
             disconnected() {
+				console.log("Disconnected from game channel.");
               // Called when the subscription has been terminated by the server
             },
 
@@ -51,7 +32,6 @@ var GameRoomInit =
       });
       return GameRoom;
     }
->>>>>>> a4492f82f837223085ad6f4857909d627ace0259
 }
 export {obtainedValues};
 export default GameRoomInit;
