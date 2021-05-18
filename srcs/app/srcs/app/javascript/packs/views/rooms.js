c7a06ba259ce156151929ee4bb9639137b096367
Gator Bright <gbright@mi-p5.msk.21-school.ru>
2021-05-18 19:52:49 +0300

    add pass check for rooms

--- srcs/app/srcs/app/javascript/packs/views/rooms.js
+++ srcs/app/srcs/app/javascript/packs/views/rooms.js
@@ -34,8 +34,10 @@ $(function () {
 		},
 		template: _.template($('#rooms-template').html()),
 		events: {
-			'click #create-room-btn' : 'create_room',
-			'click .room-click' : 'room_click',
+			'click #create-room-btn' 	: 'create_room',
+			'click .room-click' 		: 'room_click',
+			"keypress" 					: "check_keypress_event",
+			'click #new_chat'			: "click_new_chat_btn"
 		},
 		render: function () {
 			this.$el.html(this.template());
@@ -47,6 +49,8 @@ $(function () {
 			return this;
 		},
 		addOne: function (room) {
+			// if (room.get("private") == true)
+			// 	return ;
             room.view = new RoomsView.RoomView({model: room});
             this.$("#rooms").append(room.view.render().el);
         },
@@ -55,11 +59,61 @@ $(function () {
         },
 		room_click: function (e) {
 			let regex =  /\d+/;
-			let index = String(e.currentTarget)
-			index = index.substr(index.length - 1)
-			var room = this.collection.where({id: Number(index)})[0]
+			let room_id = String(e.currentTarget)
+			room_id = room_id.substr(room_id.length - 1)
+			var room = this.collection.where({id: Number(room_id)})[0]
 			
-			let view = new MessagesView.View(Number(index));
+			if (room.get("password") != "" && room.get("password") != null)
+			{
+				if ($("#input-room-password_" + room_id).css("display") == 'block')
+				{
+					$("#input-room-password_" + room_id).css("display", "none");
+					return this;
+				}
+				for (let i = 1; i <= this.collection.length; ++i)
+					$("#input-room-password_" + String(i)).css("display", "none");
+				$("#input-room-password_" + room_id).css("display", "block");
+				$('#input-room-password_' + room_id).focus();
+			}
+			else
+				this.render_messages(Number(room_id));
+			return this;
+		},
+		check_keypress_event: function (e) {
+			if (e.keyCode !== 13) return;
+
+			let target = String(e.target.id);
+			if (target.substr(0, target.length - 2) == 'input-room-password')
+				this.verify_password(Number(target.substr(target.length - 1)))
+			else if (target == 'room-name' || target == 'room-password')
+				this.create_room();
+		},
+		verify_password: function (room_id) {
+			var $this = this;
+			this.collection.fetch({
+				success: function () {
+					let password = $('#input-room-password_' + room_id).val();
+					let room = $this.collection.where({id: Number(room_id)})[0]
+					if (room.get("password") == password) {
+						$this.render_messages(room_id)
+					}
+					else {
+						Utils.appAlert('danger', {msg: 'Wrong password'});
+					}
+				}
+			})
+		},
+		click_new_chat_btn: function() {
+			if ($(".new-chat-input").css("display") == 'none')
+			{
+				$(".new-chat-input").css("display", "block");
+				this.$('#room-name').focus();
+			}
+			else
+				$(".new-chat-input").css("display", "none");
+		},
+		render_messages: function (room_id) {
+			let view = new MessagesView.View(room_id);
 			$(".app_main").html(view.render().el);
 		},
 		create_room: function () {
@@ -70,7 +124,8 @@ $(function () {
 					id: mod.cid, 
 					name: $('#room-name').val().trim(),
 					password: $('#room-password').val().trim(),
-					private: $('#is_private').prop("checked")}, {
+					private: $('#private').prop("checked")
+				}, {
 						wait: true,
 						success: function() {
 							$this.collection.fetch({
