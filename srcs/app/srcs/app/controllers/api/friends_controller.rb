8424d5a47956b1f94eeabcc2018263da7f677ea4
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-19 16:02:19 +0300

    fix friendships

--- srcs/app/srcs/app/controllers/api/friends_controller.rb
+++ srcs/app/srcs/app/controllers/api/friends_controller.rb
@@ -1,4 +1,5 @@
 class Api::FriendsController < ApplicationController
+    skip_before_action :verify_authenticity_token
     # protect_from_forgery with: :null_session
     def add_friend
         current_user.friend_request(User.find(params[:user_id]))
