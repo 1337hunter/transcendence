bf8b5abaf92eb05178aad3f2d3e22de3173309f6
Cobbler Asteria <casteria@mi-f3.msk.21-school.ru>
2021-06-02 18:49:24 +0300

    added match migration

--- srcs/app/srcs/db/schema.rb
+++ srcs/app/srcs/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_131457) do
+ActiveRecord::Schema.define(version: 2021_06_02_154827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_131457) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "winner"
     t.index ["first_player_id"], name: "index_matches_on_first_player_id"
     t.index ["second_player_id"], name: "index_matches_on_second_player_id"
   end
