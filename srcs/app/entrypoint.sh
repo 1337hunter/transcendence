0177afac9a6bf433231db5e370b2aa018fcc9e15
Ivan <thefullarcticfox@users.noreply.github.com>
2021-06-03 19:57:37 +0300

    fix entrypoint

--- srcs/app/entrypoint.sh
+++ srcs/app/entrypoint.sh
@@ -6,6 +6,6 @@ cd /app
 rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-bundle exec rake db:migrate
+bundle exec rake db:setup
 #rake db:migrate RAILS_ENV=test
 exec "$@"
