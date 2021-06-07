e42d797d888a041fd0b7124fbb017adeb2c3cefd
Ivan <thefullarcticfox@users.noreply.github.com>
2021-06-07 22:26:52 +0300

    fix

--- srcs/app/srcs/app/controllers/api/tournaments_controller.rb
+++ srcs/app/srcs/app/controllers/api/tournaments_controller.rb
@@ -4,14 +4,12 @@ class Api::TournamentsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_2fa!
   before_action :sign_out_if_banned
+  before_action :define_filters
   before_action :find_tournament, only: %i[show update destroy]
   rescue_from ActiveRecord::RecordNotFound, :with => :tournament_not_found
 
   def index
-    @tournaments = Tournament
-                     .all
-                     .includes(:users)
-    render json: @tournaments
+    render json: Tournament.all.as_json(include: {users: {only: @filters}})
   end
 
   def show
@@ -35,5 +33,10 @@ class Api::TournamentsController < ApplicationController
 
   def tournament_not_found
     render json: {error: 'Tournament not found'}, status: :not_found
+  end
+
+  def define_filters
+    @filters = %i[displayname admin banned online last_seen_at
+                  wins loses elo avatar_url avatar_default_url]
   end
 end
