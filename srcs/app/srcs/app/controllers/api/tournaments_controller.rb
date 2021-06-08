efadf8d550a42be581eec998cbbb5768c5632869
Saundra Alec <salec@il-d1.msk.21-school.ru>
2021-06-08 17:51:57 +0300

    tournaments update

--- srcs/app/srcs/app/controllers/api/tournaments_controller.rb
+++ srcs/app/srcs/app/controllers/api/tournaments_controller.rb
@@ -4,8 +4,9 @@ class Api::TournamentsController < ApplicationController
   before_action :authenticate_user!
   before_action :check_2fa!
   before_action :sign_out_if_banned
-  before_action :define_filters
+  before_action :check_admin, only: %i[create update destroy]
   before_action :find_tournament, only: %i[show update destroy]
+  before_action :define_filters
   rescue_from ActiveRecord::RecordNotFound, :with => :tournament_not_found
 
   def index
@@ -17,12 +18,24 @@ class Api::TournamentsController < ApplicationController
   end
 
   def update
+
   end
 
   def create
+    @tournament.create!(start_date: DateTime.now + 365)
   end
 
-  def delete
+  def destroy
+    @tournament.users.each { |user| user.update(tournament_id: nil) }
+    @tournament.destroy
+  end
+
+  def register_to_tournament
+
+  end
+
+  def unregister_from_tournament
+
   end
 
   private
@@ -35,8 +48,12 @@ class Api::TournamentsController < ApplicationController
     render json: {error: 'Tournament not found'}, status: :not_found
   end
 
+  def check_admin
+    render json: {error: "You have no permission"}, status: :forbidden unless current_user.admin?
+  end
+
   def define_filters
-    @filters = %i[displayname admin banned online last_seen_at nickname
-                  wins loses elo avatar_url avatar_default_url]
+    @filters = %i[id displayname admin banned online last_seen_at
+                  nickname wins loses elo avatar_url avatar_default_url]
   end
 end
