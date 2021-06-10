8931876f049667ccd8333bd070703ade2ca0c5bb
ttamesha <ttamesha@student.21-school.ru>
2021-06-11 01:26:43 +0300

    stake + and - on war win

--- srcs/app/srcs/app/jobs/war_job.rb
+++ srcs/app/srcs/app/jobs/war_job.rb
@@ -6,6 +6,12 @@ class WarJob < ApplicationJob
     @war = war.first
     if war.accepted
       @war.update(finished: true, winner: @war.define_winner)
+      if @war.winner
+        winner_guild = Guild.find(@war.winner == 1 ? guild1_id : guild2_id)
+        loser_guild = Guild.find(@war.winner == 1 ? guild2_id : guild1_id)
+        winner_guild.score += war.stake
+        loser_guild.score -= war.stake
+      end
     else
       @war.destroy
     end
