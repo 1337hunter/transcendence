fc66847b043cb8f9242a9ac9b086977a568a0f36
Cobbler Asteria <casteria@am-b3.msk.21-school.ru>
2021-06-13 01:03:58 +0300

    fix

--- srcs/app/srcs/app/channels/game_room_channel.rb
+++ srcs/app/srcs/app/channels/game_room_channel.rb
@@ -2,7 +2,7 @@ class GameRoomChannel < ApplicationCable::Channel
   def subscribed
     @match = Match.find(params[:match_id])
     stream_from "game_room_channel_#{params[:match_id]}"
-    if current_user.id == Match.find(params[:match_id]).second_player_id && @match.type != 3
+    if current_user.id == Match.find(params[:match_id]).second_player_id && @match.match_type != 3
       start_match
     end
   end
