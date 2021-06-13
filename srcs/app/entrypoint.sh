ede110a150356764dac9a7d01f51410bc5df54c2
Saundra Alec <salec@il-k5.msk.21-school.ru>
2021-06-13 17:00:08 +0300

    docker fix (waiting for pg)

--- srcs/app/entrypoint.sh
+++ srcs/app/entrypoint.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-#set -e
+#!/bin/sh
+# wait-for-postgres.sh
+set -e
+  
+until PGPASSWORD=password psql -h "postgresql" -U "postgres" -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+  
+>&2 echo "Postgres is up - executing command"
 
 cd /app
 # Remove a potentially pre-existing server.pid for Rails.
